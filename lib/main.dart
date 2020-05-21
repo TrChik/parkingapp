9350de88e53223a26c631ce6f8115dcabb369308
tunge-munge <oleksiytverd@gmail.com>
2020-05-22 00:23:57 +0300

    f
    f

--- lib/main.dart
+++ lib/main.dart
@@ -29,18 +29,36 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       drawer: NavDrawer(),
       appBar: AppBar(
-        title: Text('trying get stuff to work here ok'),
+        title: Text('try better'),
       ),
       body: SizedBox.expand(
         child: DraggableScrollableSheet(
+          initialChildSize: 0.04,
+          minChildSize: 0.04,
           builder: (BuildContext context, ScrollController scrollController) {
-            return Container(
-              color: Colors.blue[100],
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'yes',
-              ),
+            return ClipRRect(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
+                child: Container(
+                    color: Colors.red[100],
+                    child: ListView.builder(
+                      controller: scrollController,
+                      itemCount: 10,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(title: Text('Item $index'));
+                      },
+                    )
+                )
             );
+            // return Container(
+            //   color: Colors.blue[100],
+            //   child: ListView.builder(
+            //     controller: scrollController,
+            //     itemCount: 25,
+            //     itemBuilder: (BuildContext context, int index) {
+            //       return ListTile(title: Text('Item $index'));
+            //     },
+            //   ),
+            // );
           },
         ),
       ),
