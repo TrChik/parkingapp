055d3a5adbb0b4110dc75bfa68bbe366e78b6faa
tunge-munge <oleksiytverd@gmail.com>
2020-05-22 15:08:11 +0300

    а
    а

--- lib/main.dart
+++ lib/main.dart
@@ -41,17 +41,36 @@ class MyHomePage extends StatelessWidget {
           builder: (BuildContext context, ScrollController scrollController) {
           double scrollableSheetDragBarHeight = MediaQuery.of(context).size.height * 0.1;
             return ClipRRect(
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(20,), topRight: Radius.circular(20.0)),
+
               child: Container(
+
+
                 color: Colors.blue[100],
                 child: ListView(
-                  controller: scrollController,
+             controller: scrollController,
                   children: <Widget>[
+                    Container( // нашей кнопкой будет контейнер
+                      decoration: BoxDecoration( // стилизуем контейнер
+                        shape: BoxShape.circle, // зададим ему круглую форму
+                        color: Color(0xFF17A2B8), // и покрасим его в синий
+                      ),
+                      width: 20.0,
+                      height: 20.0,
+                    ),
                     Container(
                       color: Colors.blue[600],
-                      height: scrollableSheetDragBarHeight      // here, you need to add that white cool thingie
-                    ),
-                    ListTile(title: Text('no')),
+                      height: scrollableSheetDragBarHeight  ,    // here, you need to add that white cool thing
+
+                      child:Center(
+
+                    child: Text('info'),
+                      ),
+
+            ),
+
+                    ListTile(title: Text('nein')),
+
                   ]
                 )
               )
