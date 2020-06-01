3e4af07a9d9f65ec3a0f205f2990cb071387e8e5
TrChik <trchik14@gmail.com>
2020-06-01 17:22:01 +0300

    image + 1 button

--- lib/main.dart
+++ lib/main.dart
@@ -39,58 +39,86 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('trying get stuff to work here ok'),
       ),
-      body: SizedBox.expand(
-        child: DraggableScrollableSheet(
-          initialChildSize: 0.05,
-          minChildSize: 0.05,
-          builder: (BuildContext context, ScrollController scrollController) {
-          double scrollableSheetDragBarHeight = MediaQuery.of(context).size.height * 0.1;
-            return ClipRRect(
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
-              child: Container(
-                color: Colors.blue[100],
-                child: ListView(
-                  controller: scrollController,
-                  children: <Widget>[
-                    Container(
-                      color: Colors.blue[600],
-                      height: scrollableSheetDragBarHeight,      
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            height: scrollableSheetDragBarHeight/2,
-                            child: Center(
-                              child: Container(
-                                width: 100,
-                                height: scrollableSheetDragBarHeight/6,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.all(Radius.circular(20)),
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(5),
+                color: Colors.black,
+                child: Image.asset(
+                  'images/imagescheme.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 10),
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.blue[400],
+                  child: Container(
+                    padding: EdgeInsets.only(left: 100, right: 100),
+                    child: Text('View list')
+                  )
+                )
+              ),
+            ],
+          ),
+          SizedBox.expand(
+            child: DraggableScrollableSheet(
+              initialChildSize: 0.05,
+              minChildSize: 0.05,
+              builder: (BuildContext context, ScrollController scrollController) {
+              double scrollableSheetDragBarHeight = MediaQuery.of(context).size.height * 0.1;
+                return ClipRRect(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
+                  child: Container(
+                    color: Colors.blue[100],
+                    child: ListView(
+                      controller: scrollController,
+                      children: <Widget>[
+                        Container(
+                          color: Colors.blue[600],
+                          height: scrollableSheetDragBarHeight,      
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                height: scrollableSheetDragBarHeight/2,
+                                child: Center(
                                   child: Container(
-                                    color: Colors.black45,
+                                    width: 100,
+                                    height: scrollableSheetDragBarHeight/6,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                                      child: Container(
+                                        color: Colors.black45,
+                                      )
+                                    )
                                   )
                                 )
+                              ),
+                              Container(
+                                height: scrollableSheetDragBarHeight/2,
+                                padding: EdgeInsets.only(top: 7),
+                                child: Text(
+                                  'Info',
+                                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20)
+                                ),
                               )
-                            )
-                          ),
-                          Container(
-                            height: scrollableSheetDragBarHeight/2,
-                            padding: EdgeInsets.only(top: 7),
-                            child: Text(
-                              'Info',
-                              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20)
-                            ),
+                            ],
                           )
-                        ],
-                      )
-                    ),
-                    ListTile(title: Text('no')),
-                  ]
-                )
-              )
-            );
-          },
-        ),
-      ),
+                        ),
+                        ListTile(title: Text('no')),
+                      ]
+                    )
+                  )
+                );
+              },
+            ),
+          ),
+        ],
+      )
     );
   }
 }
